98db9689f30b4d5f5a2f212376d652a07ac8e013
anagoncalves2002 <margarisagoncalves@gmail.com>
2023-05-29 20:16:07 +0100

    codigo

--- sketch.js
+++ sketch.js
@@ -1,23 +1,37 @@
-let espLarg = 20;
-let espAltura = 20;
+let espLarg = 15;
+let espAltura = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background(160);
   stroke(255);
   strokeWeight(2);
   noFill();
-  background(0);
+  background(160);
 }
 
 function draw() {
   
-  for (let i = 1; i < espLarg; i++) {
-    for (let j = 1; j < espAltura; j++) {
-      push();
-      translate(i * width / espLarg,j * height / espAltura)
-      circle(0, 0, height / espAltura);
-      pop();
+	for (let i = 1; i < espLarg; i++) {
+	  for (let j = 1; j < espAltura; j++) {
+		push();
+		translate(i * width / espLarg, j * height / espAltura)
+		translate(-55,-45)
+		scale(0.08);
+		noStroke();
+
+
+	noFill();
+	stroke(30,255);
+	circle(width / 2, height / 2, height / 2 );
+	circle(width / 2 - height /3/4, height /2 - height/18, height/12 );
+	circle(width / 2 + height /3/4, height /2 - height/18, height/12 );
+	line(850, 500, 700, 500);
+	ellipse(760, 670, 210, 242);
+	ellipse(920, 580, 170, 52);
+	ellipse(600, 580, 170, 52);
+	 
+	  pop();
     }
   }
 }
